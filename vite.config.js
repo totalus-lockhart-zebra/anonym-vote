7e3dcf4f1ae70e9288d2951a4b6d02263d190317
Evgeny Svirsky <e.svirsky@tut.by>
2026-04-09 14:41:58 +0200

    fix prefix

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
+  base: '/anonym-vote/',
   define: {
     // Required for @polkadot packages in browser
-    global: "globalThis",
+    global: 'globalThis',
   },
 });
